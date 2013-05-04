e43f2bde8f13b403de721f6959e6a98ed3c7648f
Ryan Kahn <ryan@ryan-kahn.com>
2013-05-04 10:43:48 -0400

    Updated game engine

--- public/coffeescripts/game.coffee
+++ public/coffeescripts/game.coffee
@@ -10,26 +10,45 @@ getRandomArbitary = (min, max)->
 
 
 # And turn on default input controls and touch input (for UI)
-Q = window.Q = Quintus().include("Sprites, Scenes, Input, 2D, Anim, Touch, UI").setup(maximize: true).controls().touch()
+Q = window.Q = Quintus().include("Sprites, Scenes, Input, 2D, Anim, Touch, UI")
+Q.setup(maximize: true).controls().touch(Q.SPRITE_ALL)
 
 # ## Enemy Sprite
 # Create the Enemy class to add in some baddies
 Q.Sprite.extend "Enemy",
   init: (p) ->
-
-    # Get a number between .5 and .1
-    p.scale = getRandomArbitary(.5,.1)
     p.vx = getRandomArbitary(0,400)
     if p.x > 400
       p.vx *= -1
+
     @_super p,
       vx: getRandomArbitary(-400,400)
       vy:-1300
+      scale: getRandomArbitary(.5,.1)
       sheet: "enemy"
+
+    @on 'touch'
 
     # Enemies use the Bounce AI to change direction
     # whenver they run into something.
-    @add "2d"
+
+    @add "2d, tween"
+
+  touch: (data)->
+    if !@dead
+      @dead = true
+      angleX = data.oldLocation.x - data.location.x
+      angleY = data.oldLocation.y - data.location.y
+
+      @p.vy -= angleY*50
+      @p.vx -= angleX*50
+
+      fadeOutTime = .5
+      @animate({angle: 1720, scale: .001}, fadeOutTime, Q.Easing.Linear)
+      window.setTimeout =>
+        @destroy()
+      , fadeOutTime * 1000
+
 
 
 
@@ -46,7 +65,33 @@ Q.scene "level1", (stage) ->
   )
 
   # Create the player and add them to the stage
-  enemy = stage.insert new Q.Enemy
+
+  console.log stage
+  stage.insert new Q.Enemy
+    x: getRandomArbitary(0,800)
+    y: 1000
+  stage.insert new Q.Enemy
+    x: getRandomArbitary(0,800)
+    y: 1000
+  stage.insert new Q.Enemy
+    x: getRandomArbitary(0,800)
+    y: 1000
+  stage.insert new Q.Enemy
+    x: getRandomArbitary(0,800)
+    y: 1000
+  stage.insert new Q.Enemy
+    x: getRandomArbitary(0,800)
+    y: 1000
+  stage.insert new Q.Enemy
+    x: getRandomArbitary(0,800)
+    y: 1000
+  stage.insert new Q.Enemy
+    x: getRandomArbitary(0,800)
+    y: 1000
+  stage.insert new Q.Enemy
+    x: getRandomArbitary(0,800)
+    y: 1000
+  stage.insert new Q.Enemy
     x: getRandomArbitary(0,800)
     y: 1000
 
@@ -63,3 +108,36 @@ Q.load "background-wall.png, enemy.png", ->
 
   # Finally, call stageScene to run the game
   Q.stageScene "level1"
+
+  # Touch events do most of the work for us, but the
+  # touch system doesn't handle mousemouse events, so lets add
+  # in an event listener and use `Stage.locate` to highlight
+  # sprites on desktop.
+  oldLocation =
+    x: 0
+    y: 0
+  Q.el.addEventListener "mousemove", (e) ->
+    x = e.offsetX or e.layerX
+    y = e.offsetY or e.layerY
+    stage = Q.stage()
+
+    # Use the helper methods from the Input Module on Q to
+    # translate from canvas to stage
+    stageX = Q.canvasToStageX(x, stage)
+    stageY = Q.canvasToStageY(y, stage)
+
+    # Find the first object at that position on the stage
+    obj = stage.locate(stageX, stageY)
+
+    if obj?.touch?
+      # Set a `hit` property so the step method for the
+      # sprite can handle scale appropriately
+      obj.touch
+        location:
+          x: x
+          y: y
+        oldLocation: oldLocation
+
+    oldLocation =
+      x: x
+      y: y
--- public/javascripts/game.js
+++ public/javascripts/game.js
@@ -5,43 +5,124 @@
     return Math.random() * (max - min) + min;
   };
 
-  Q = window.Q = Quintus().include("Sprites, Scenes, Input, 2D, Anim, Touch, UI").setup({
+  Q = window.Q = Quintus().include("Sprites, Scenes, Input, 2D, Anim, Touch, UI");
+
+  Q.setup({
     maximize: true
-  }).controls().touch();
+  }).controls().touch(Q.SPRITE_ALL);
 
   Q.Sprite.extend("Enemy", {
     init: function(p) {
-      p.scale = getRandomArbitary(.5, .1);
       p.vx = getRandomArbitary(0, 400);
       if (p.x > 400) p.vx *= -1;
       this._super(p, {
         vx: getRandomArbitary(-400, 400),
         vy: -1300,
+        scale: getRandomArbitary(.5, .1),
         sheet: "enemy"
       });
-      return this.add("2d");
+      this.on('touch');
+      return this.add("2d, tween");
+    },
+    touch: function(data) {
+      var angleX, angleY, fadeOutTime;
+      var _this = this;
+      if (!this.dead) {
+        this.dead = true;
+        angleX = data.oldLocation.x - data.location.x;
+        angleY = data.oldLocation.y - data.location.y;
+        this.p.vy -= angleY * 50;
+        this.p.vx -= angleX * 50;
+        fadeOutTime = .5;
+        this.animate({
+          angle: 1720,
+          scale: .001
+        }, fadeOutTime, Q.Easing.Linear);
+        return window.setTimeout(function() {
+          return _this.destroy();
+        }, fadeOutTime * 1000);
+      }
     }
   });
 
   Q.scene("level1", function(stage) {
-    var enemy;
     stage.insert(new Q.Repeater({
       asset: "background-wall.png",
       speedX: 0.5,
       speedY: 0.5
     }));
-    return enemy = stage.insert(new Q.Enemy({
+    console.log(stage);
+    stage.insert(new Q.Enemy({
+      x: getRandomArbitary(0, 800),
+      y: 1000
+    }));
+    stage.insert(new Q.Enemy({
+      x: getRandomArbitary(0, 800),
+      y: 1000
+    }));
+    stage.insert(new Q.Enemy({
+      x: getRandomArbitary(0, 800),
+      y: 1000
+    }));
+    stage.insert(new Q.Enemy({
+      x: getRandomArbitary(0, 800),
+      y: 1000
+    }));
+    stage.insert(new Q.Enemy({
+      x: getRandomArbitary(0, 800),
+      y: 1000
+    }));
+    stage.insert(new Q.Enemy({
+      x: getRandomArbitary(0, 800),
+      y: 1000
+    }));
+    stage.insert(new Q.Enemy({
+      x: getRandomArbitary(0, 800),
+      y: 1000
+    }));
+    stage.insert(new Q.Enemy({
+      x: getRandomArbitary(0, 800),
+      y: 1000
+    }));
+    return stage.insert(new Q.Enemy({
       x: getRandomArbitary(0, 800),
       y: 1000
     }));
   });
 
   Q.load("background-wall.png, enemy.png", function() {
+    var oldLocation;
     Q.sheet("enemy", "enemy.png", {
       tilew: 300,
       tileh: 240
     });
-    return Q.stageScene("level1");
+    Q.stageScene("level1");
+    oldLocation = {
+      x: 0,
+      y: 0
+    };
+    return Q.el.addEventListener("mousemove", function(e) {
+      var obj, stage, stageX, stageY, x, y;
+      x = e.offsetX || e.layerX;
+      y = e.offsetY || e.layerY;
+      stage = Q.stage();
+      stageX = Q.canvasToStageX(x, stage);
+      stageY = Q.canvasToStageY(y, stage);
+      obj = stage.locate(stageX, stageY);
+      if ((obj != null ? obj.touch : void 0) != null) {
+        obj.touch({
+          location: {
+            x: x,
+            y: y
+          },
+          oldLocation: oldLocation
+        });
+      }
+      return oldLocation = {
+        x: x,
+        y: y
+      };
+    });
   });
 
 }).call(this);
